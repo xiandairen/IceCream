1dd09c6c1327914561d068c519991f44de41271e
xiandairen <819812520@qq.com>
2014-12-24 10:53:54 +0800

    提交js效果

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 		$that.on("click",function() {
 			switch($i){
 				case 0:
-					goTop(0);
+					goTop();
 					break;
 				case 1:
 				     goTop();
@@ -37,16 +37,46 @@ $(document).ready(function(){
 		});
 
 		function goTop(){
-			$topUlLi.removeClass("nav-hover-a").eq($i-1).addClass("nav-hover-a")
-			if($i==1){
-				$topMain.stop().css('top', '0');//顶部的导航回到顶部;
-				$Wrap.stop().css("top","0")
+			$topUlLi.removeClass("nav-hover-a").eq($i-1).addClass("nav-hover-a");
+			if($i == 1){
+				$topMain.removeClass("top-main-pis");
+				$(window).scrollTop(0)
 			}else{
-				$topMain.stop().css('top', '-748px');//顶部的导航回到顶部;
-				$Wrap.stop().animate({"top":-($i*822)+75}, "slow")
+				$topMain.addClass("top-main-pis");
+				switch ($i){
+					case 2:
+						$(window).scrollTop(760);
+						break;
+					case 3:
+						$(window).scrollTop(1582);
+						break;
+					case 4:
+						$(window).scrollTop(2405);
+						break;
+					case 5:
+						$(window).scrollTop(3230);
+						break;
+					case 6:
+						$(window).scrollTop(4050);
+						break;
+					case 7:
+						$(window).scrollTop(4870);
+						break;
+				}
+
 			}
 		}
 
+		$(window).scroll(function(){
+			console.log($(window).scrollTop);
+
+		})
+
+		//回到顶部
+		$("#goUp").click(function(){
+
+
+		})
 		
 
 
